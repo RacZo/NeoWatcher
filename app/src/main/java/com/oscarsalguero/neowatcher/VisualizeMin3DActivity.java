9ca235d058ca379e5a8036af22e3ef6df78a813f
Oscar S <osg@realactionsoft.com>
2016-04-24 13:26:03 -0400

    Added asteroid to 3D visualization Created image composition in Photoshop Added screenshots Updated README

--- app/src/main/java/com/oscarsalguero/neowatcher/VisualizeMin3DActivity.java
+++ app/src/main/java/com/oscarsalguero/neowatcher/VisualizeMin3DActivity.java
@@ -2,6 +2,9 @@ package com.oscarsalguero.neowatcher;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.oscarsalguero.neowatcher.bean.PotentiallyHazardousAsteroid;
 
 import min3d.Shared;
 import min3d.Utils;
@@ -17,15 +20,35 @@ import min3d.vos.Light;
  */
 public class VisualizeMin3DActivity extends RendererActivity {
 
+    private static final String LOG_TAG = VisualizeMin3DActivity.class.getName();
+
+    public final static String PARAM_NEO = "PARAM_NEO";
+
     Object3dContainer _sun;
     Object3dContainer _earth;
     Object3dContainer _moon;
+    Object3dContainer _pha;
 
     int _count;
+
+    private PotentiallyHazardousAsteroid mPHA;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            mPHA = (PotentiallyHazardousAsteroid) bundle.getSerializable(PARAM_NEO);
+        }
+
+        if (mPHA != null) {
+            String name = mPHA.getName();
+            if (TextUtils.isEmpty(name)) {
+                name = mPHA.getPrincipalDesignation();
+            }
+            setTitle(name + " Visualization");
+        }
     }
 
     @Override
@@ -56,9 +79,20 @@ public class VisualizeMin3DActivity extends RendererActivity {
         _moon.position().x = 0.6f;
         _earth.addChild(_moon);
 
+
+        // Add PHA as a child of the Sun
+        _pha = new Sphere(0.1f, 20, 11, true, true, false);
+        _pha.position().x = 8.0f;
+        _pha.rotation().x = 1;
+        _sun.addChild(_pha);
+
         // Add textures to TextureManager
         Bitmap b = Utils.makeBitmapFromResourceId(this, R.drawable.sun);
         Shared.textureManager().addTextureId(b, "sun", false);
+        b.recycle();
+
+        b = Utils.makeBitmapFromResourceId(this, R.drawable.asteroid);
+        Shared.textureManager().addTextureId(b, "asteroid", false);
         b.recycle();
 
         b = Utils.makeBitmapFromResourceId(this, R.drawable.earth);
@@ -73,6 +107,7 @@ public class VisualizeMin3DActivity extends RendererActivity {
         _sun.textures().addById("sun");
         _earth.textures().addById("earth");
         _moon.textures().addById("moon");
+        _pha.textures().addById("asteroid");
 
         _count = 0;
 
@@ -85,6 +120,7 @@ public class VisualizeMin3DActivity extends RendererActivity {
         _sun.rotation().y += 0.5f;
         _earth.rotation().y += 3.0f;
         _moon.rotation().y -= 3.0f;
+        _pha.rotation().y += 1.0f;
 
         // Wobble Sun a little just for fun
         _count++;
@@ -92,7 +128,7 @@ public class VisualizeMin3DActivity extends RendererActivity {
         _sun.rotation().z = (float) Math.sin(_count * .33 * Utils.DEG) * mag;
 
         // Move camera around
-        scene.camera().position.z = 10.5f + (float) Math.sin(_sun.rotation().y * Utils.DEG);
+        scene.camera().position.z = 12.5f + (float) Math.sin(_sun.rotation().y * Utils.DEG);
         scene.camera().target.x = (float) Math.sin((_sun.rotation().y + 90) * Utils.DEG) * 0.8f;
 
     }
